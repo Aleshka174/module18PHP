ba5184dc77b06d32ba596de27892e468dab418d1
Aleshka174 <alexey20.05@inbox.ru>
2020-12-06 12:34:37 +0500

    first change

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('URL', 'http://localhost:8080/Task%2018%20myExample/'); // URL текущей страницы(моя директория)
+define('UPLOAD_MAX_SIZE', 1000000); // 1mb
+define('ALLOWED_TYPES', ['image/jpeg', 'image/png', 'image/gif']);
+define('UPLOAD_DIR', 'gallery');
+define('COMMENT_DIR', 'comments');
